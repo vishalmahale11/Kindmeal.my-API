31455e844c3479793af9415eb1ea6e8ef6049a99
vishalmahle11 <vishalmahale974@gmail.com>
2022-12-25 17:59:58 +0530

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+const cors = require("cors");
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
